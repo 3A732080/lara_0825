2d5d63b10d9ea989dc3bd6a1483c55311d478d40
3A732080 <tiffany89031176@gmail.com>
2020-10-31 00:12:54 +0800

    修改主樣板內 script 標籤 當中 js 檔案的參考位置 ( url)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,13 +35,15 @@
   <!-- Footer -->
   @include('layouts.partials.footer')
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.min.js"></script>
-
+  <!-- jQuery -->
+  <script src="{{asset('js/jquery.min.js')}}"></script>
+  <!-- Bootstrap Core JavaScript -->
+  <script src="{{asset('js/bootstrap.min.js')}}"></script>
+  <!-- Contact Form JavaScript -->
+  <script src="{{asset('js/jqBootstrapValidation.js')}}"></script>
+  <script src="{{asset('js/contact_me.js')}}"></script>
+  <!-- Theme JavaScript -->
+  <script src="{{asset('js/clean-blog.min.js')}}"></script>
 </body>
 
 </html>
