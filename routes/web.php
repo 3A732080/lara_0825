3940bc65bc126703b4f8611db70cac7da73253bb
3A732080 <tiffany89031176@gmail.com>
2020-11-13 01:17:01 +0800

    lara_0825 練習5：刪除資料(特定幾筆)

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,10 @@ Route::get('/', function () {
     /*$post=Post::find(1);
     $post->delete();*/
 
-    Post::destroy(2);
+    //刪除資料(destory)
+    //Post::destroy(2);
+
+    Post::destroy(3,5,7);
 });
 Route::get('posts', ['as' => 'posts.index', 'uses' =>
     'PostsController@index']);
